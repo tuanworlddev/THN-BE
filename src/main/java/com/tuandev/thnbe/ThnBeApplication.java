2c8c1bdecbc57796da067fd1d3b77b80e3f93089
tuanworlddev <nanhtuan.it@gmail.com>
2024-09-08 08:40:05 +0700

    first commit

---
+++ src/main/java/com/tuandev/thnbe/ThnBeApplication.java
@@ -0,0 +1,13 @@
+package com.tuandev.thnbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThnBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuandev/thnbe/ThnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandev.thnbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
